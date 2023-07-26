e570713402c6ac2afe8585648b9ac53a24984cdb
GwendiWisnuAnggoro <105532618+GwendiWisnuAnggoro@users.noreply.github.com>
2023-07-26 20:49:22 +0700

    .

--- framework/AmbilDataGoogleSheets.js
+++ framework/AmbilDataGoogleSheets.js
@@ -33,3 +33,45 @@ class AmbilData {
         })
     }
 }
+
+class EditValues {
+    constructor(column, row, newValue) {
+      this.edit(column, row, newValue);
+    }
+  
+    edit(column, row, value) {
+      // Convert column letter to column index (A=1, B=2, etc.)
+      var columnIndex = column.toUpperCase().charCodeAt(0) - 64;
+  
+      // Ganti URL berikut dengan URL aplikasi web yang Anda dapatkan setelah deploy
+      var url = "https://script.google.com/macros/s/AKfycbxx-yYbDTGQ38uztXZr8Gcy1cY_TPpCz1y7yUSHYUqlT2O-Xv8RGcCzRj0a5LAHr_2o/exec";
+  
+      // Buat objek data untuk dikirimkan sebagai permintaan POST
+      var data = {
+        column: columnIndex,
+        row: row,
+        newValue: value
+      };
+  
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        // Jangan gunakan 'no-cors' mode karena Anda ingin mendapatkan respons dari server
+        // Ganti ke 'cors' mode agar dapat mengakses respons dari server.
+        mode: 'no-cors', // Ganti 'no-cors' menjadi 'cors'
+        body: JSON.stringify(data)
+      })
+      .then(response => {
+        console.log("Cell value updated!");
+        console.log(response);
+      })
+      .catch(error => {
+        console.error("Error updating cell value:", error);
+      });
+    }
+  }
+  
+
+  
--- script.js
+++ script.js
@@ -1,32 +1,154 @@
+// JavaScript
 const idSheets = "1kMfONdlnYgFyR25YmZ3QKrAjE9HHVsmyrfPjwZ3rke8";
 
 // Buat instance dari kelas AmbilData dengan kolom yang sesuai
-let nama = new AmbilData(idSheets, "B");
-let kas = new AmbilData(idSheets, "C");
-let kurang = new AmbilData(idSheets, "D");
+let nama_code = "B",
+kas_code = "C",
+kurang_code = "D",
+tambah_kurang_code = "E";
+let nama = new AmbilData(idSheets, nama_code);
+let kas = new AmbilData(idSheets, kas_code);
+let kurang = new AmbilData(idSheets, kurang_code);
+let edit = new AmbilData(idSheets, tambah_kurang_code);
 const totalKas = 52 * 2000;
+let list = ""; // Variabel list sebagai variabel global
+let searchText = ""; // Variabel untuk menyimpan nilai dari input search
+const code_Admin = "mse1683y24r3q2fk1c8daniq58pxftzp";
+let mode_Admin = false;
 
-// Panggil fungsi tampilkan untuk menampilkan data
-tampilkan();
+// Ambil elemen input dengan id "cari"
+const btnSearch = document.getElementById("button-addon2");
+const searchInput = document.getElementById("cari");
 
-function tampilkan(){
-    // Tunggu hingga data berhasil diambil (fetch selesai)
-    setInterval(() => {
-        let list = ""; // Variabel list diinisialisasi dengan string kosong
+// Tambahkan event listener untuk click event pada btnSearch
+btnSearch.addEventListener("click", () => {
+  // Panggil fungsi tampilkan untuk menampilkan data sesuai dengan pencarian
+  searchText = searchInput.value;
+  if (searchText.includes(code_Admin)) {
+    mode_Admin = true;
+    document.querySelector(".kurangi_tmbh").style.display = "block";
+    
+    if (searchText.includes(`${code_Admin}#`)) {
+      searchText = searchText.substring(searchText.indexOf('#') + 1).toLowerCase();
+    } else {
+      searchText = "";
+    }
+  } else {
+    mode_Admin = false;
+    document.querySelector(".kurangi_tmbh").style.display = "none";
+  }
+  tampilkan();
+});
 
-        for (let i = 1; i < nama.result.length; i++){
-            // Gunakan indeks i untuk mengakses nilai pada setiap iterasi
-            list += `
-            <li>
-                <div>
-                    <h1>${nama.result[i].value}</h1>
-                    <p>Kamu Kas: ${kas.result[i].value}</p>
-                    <p>Kamu Kurang: ${kurang.result[i].value}</p>
-                </div>
-            </li>`;
+
+// Tunggu hingga data berhasil diambil (fetch selesai)
+let dataFetchInterval = setInterval(() => {
+  tampilkan(); // Update the data display periodically
+}, 500); // Ganti angka 500 dengan waktu yang sesuai untuk menunggu fetch selesai
+
+function tampilkan() {
+  // Tampilkan data sesuai dengan pencarian
+  list = ""; // Variabel list diinisialisasi dengan string kosong
+  
+  if (searchText) {
+    // Ambil nilai dari input search
+    let currentSearchText = searchText.toLowerCase();
+  const searchInputs = document.getElementById('cari');
+  let previousLength = searchInput.value.length;
+  
+  searchInputs.addEventListener('input', () => {
+      const currentLength = searchInputs.value.length;
+
+      if (currentLength < previousLength) {
+        searchText = searchInputs.value;
+      }
+  
+      // Update panjang teks sebelumnya untuk perbandingan berikutnya.
+      previousLength = currentLength;
+  });
+  
+
+  // Jika ada perubahan pada input search, update searchText
+  if (currentSearchText !== searchText) {
+    searchText = currentSearchText;
+  }
+    for (let i = 1; i < nama.result.length; i++) {
+      const namaValue = nama.result[i].value.toLowerCase();
+      if (namaValue.includes(searchText)) {
+        if(!mode_Admin){
+          list += `
+          <li class="list-group-item">
+            <h1>${nama.result[i].value}</h1>
+            <p>Kas Kamu Saat Ini: ${kas.result[i].value}</p>
+            <p>Kamu Kurang: ${kurang.result[i].value}</p>
+          </li>`;
+        } else {
+          list += `
+          <li class="list-group-item">
+            <h1>${nama.result[i].value}</h1>
+            <p>Kas Kamu Saat Ini: ${kas.result[i].value}</p>
+            <p>Kamu Kurang: ${kurang.result[i].value}</p>
+            <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+              <button type="button" class="btn btn-success" id="${nama.result[i].rows}" onclick="tambah(this, ${edit.result[i].value})">+</button>
+              <button type="button" class="btn btn-danger" id="${nama.result[i].rows}" onclick="kurangi(this, ${edit.result[i].value})">-</button>
+            </div>
+          </li>`;
         }
+      }
+    }
+  } else {
+    // Jika tidak ada input search, tampilkan semua data
+    for (let i = 1; i < nama.result.length; i++) {
+      if(!mode_Admin){
+        list += `
+        <li class="list-group-item">
+          <h1>${nama.result[i].value}</h1>
+          <p>Kas Kamu Saat Ini: ${kas.result[i].value}</p>
+          <p>Kamu Kurang: ${kurang.result[i].value}</p>
+        </li>`;
+      } else {
+        list += `
+        <li class="list-group-item">
+          <h1>${nama.result[i].value}</h1>
+          <p>Kas Kamu Saat Ini: ${kas.result[i].value}</p>
+          <p>Kamu Kurang: ${kurang.result[i].value}</p>
+          <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+            <button type="button" class="btn btn-success" id="${nama.result[i].rows}" onclick="tambah(this, ${edit.result[i].value})">+</button>
+            <button type="button" class="btn btn-danger" id="${nama.result[i].rows}" onclick="kurangi(this, ${edit.result[i].value})">-</button>
+          </div>
+        </li>`;
 
-        // Setelah loop selesai, tambahkan semua konten ke dalam elemen <ul>
-        document.getElementById("uls").innerHTML = list;
-    }, 500); // Ganti angka 2000 dengan waktu yang sesuai untuk menunggu fetch selesai
+      }
+    }
+  }
+
+  // Setelah loop selesai, tambahkan semua konten ke dalam elemen <ul>
+  document.getElementById("kas_list").innerHTML = list;
+}
+
+
+function tambah(v, data){
+  let rows = v.id,
+  code = tambah_kurang_code,
+  dataSekarang = parseInt(data),
+  tmbh_krng = document.getElementById("tmbh_krng");
+  
+  let tambah = (dataSekarang + tmbh_krng.value).toString();
+  new EditValues(code, rows, tambah)
+  tmbh_krng.value = 0
+}
+
+function kurangi(v, data){
+  let rows = v.id,
+  code = tambah_kurang_code,
+  dataSekarang = parseInt(data),
+  tmbh_krng = document.getElementById("tmbh_krng");
+
+  let kurang = (dataSekarang - tmbh_krng.value);
+  if(kurang < 0){
+    new EditValues(code, rows, "0")
+  } else {
+    new EditValues(code, rows, kurang.toString())
+  }
+  tmbh_krng.value = 0
 }
